8c0bfe2605fb7a79f19dfdab59b951af3acee132
Arseniy Garelyshev <Ars.garelyshev@yandex.ru>
2018-04-09 15:39:15 +0500

    Simplification

--- Robots-master/robots/src/gui/MainApplicationFrame.java
+++ Robots-master/robots/src/gui/MainApplicationFrame.java
@@ -6,19 +6,10 @@ import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.*;
-
 import javax.swing.*;
 
-import com.sun.rmi.rmid.ExecPermission;
 import log.Logger;
-import sun.applet.Main;
 
-/**
- * Что требуется сделать:
- * 1. Метод создания меню перегружен функционалом и трудно читается. 
- * Следует разделить его на серию более простых методов (или вообще выделить отдельный класс).
- *
- */
 public class MainApplicationFrame extends JFrame
 {
     private final JDesktopPane desktopPane = new JDesktopPane();
@@ -28,10 +19,7 @@ public class MainApplicationFrame extends JFrame
         //of the screen.
         int inset = 50;        
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        setBounds(inset, inset,
-            screenSize.width  - inset*2,
-            screenSize.height - inset*2);
-
+        setBounds(inset, inset, screenSize.width  - inset*2, screenSize.height - inset*2);
         setContentPane(desktopPane);
 
         read();
@@ -46,7 +34,32 @@ public class MainApplicationFrame extends JFrame
         });
     }
 
-    protected void closeAll() {
+    private void read() {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("pos.txt"));
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(" ");
+                int converted[] = new int[4];
+                for (int i = 0; i < 4; i++)
+                    converted[i] = Integer.parseInt(parts[i]);
+                switch (parts[5]) {
+                    case "log":
+                        addWindow(createLogWindow(converted[1],converted[2],converted[3],converted[4]));
+                        break;
+                    case "game":
+                        addWindow(createGameWindow(converted[1],converted[2],converted[3],converted[4]));
+                        break;
+                }
+            }
+        } catch (Exception e) {
+            createDefaultPair();
+            Logger.debug("Ошибка: " + e.toString());
+            Logger.debug("Созданы окна по-умолчанию.");
+        }
+    }
+
+    private void closeAll() {
         Object[] options = {"Да", "Нет"};
         int dialog = new JOptionPane().showOptionDialog( this,
                 "Вы уверены, что хотите выйти?",
@@ -57,108 +70,62 @@ public class MainApplicationFrame extends JFrame
                 options,
                 options[1]);
         if (dialog == JOptionPane.YES_OPTION) {
-            //clear pos.txt
-            Component[] components = getContentPane().getComponents();
-            for (Component component : components) {
-                close(component);
+            try {
+                BufferedWriter br = new BufferedWriter(new FileWriter("pos.txt"));
+                Component[] components = getContentPane().getComponents();
+                for (Component component : components) {
+                    br.append(component.getX() + " " + component.getY() + " "
+                            + component.getWidth() + " " + component.getHeight() + " "
+                            + component.getName() + "\n");
+                }
+                br.flush();
+                br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
             System.exit(0);
         }
     }
 
-
-    protected void close(Component component) {
-        try {
-            BufferedWriter br = new BufferedWriter(new FileWriter("pos.txt", true));
-            br.write(component.getName() + " " + component.getX() + " " + component.getY() + " "
-                    + component.getWidth() + " " + component.getHeight());
-            br.newLine();
-            br.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    protected void read() {
-        try {
-            BufferedReader br = new BufferedReader(new FileReader("pos.txt"));
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] parts = line.split(" ");
-                int converted[] = new int[5];
-                for (int i = 0; i < 5; i++)
-                    converted[i] = Integer.parseInt(parts[i]);
-                if (converted[0] == 0) { // 0 - log
-                    LogWindow logWindow = createLogWindow(converted[1],converted[2],converted[3],converted[4]);
-                    addWindow(logWindow);
-                    logWindow.setName("0");
-                }
-                if (converted[0] == 1) { // 1 - game
-                    GameWindow gameWindow = createGameWindow(converted[1],converted[2],converted[3],converted[4]);
-                    addWindow(gameWindow);
-                    gameWindow.setName("1");
-                }
-            }
-        } catch (Exception e) {
-            createDefaultPair();
-            Logger.debug("Ошибка чтения, созданы окна по-умолчанию.");
-        } finally {
-            File f = new File("pos.txt");
-            try {
-                PrintWriter writer = new PrintWriter(f);
-                writer.close();
-            } //clearing file and ignoring if doesn't exist.
-            catch (FileNotFoundException e) {}
-        }
-    }
-
-    protected LogWindow createLogWindow()
-    {
+    private LogWindow createLogWindow(int x, int y, int width, int height) {
         LogWindow logWindow = new LogWindow(Logger.getDefaultLogSource());
-        logWindow.setLocation(10,10);
-        logWindow.setSize(300, 800);
-        setMinimumSize(logWindow.getSize());
-        logWindow.pack();
-        Logger.debug("Протокол работает");
-        return logWindow;
-    }
-
-    protected LogWindow createLogWindow(int x, int y, int width, int heigh) {
-        LogWindow logWindow = new LogWindow(Logger.getDefaultLogSource());
+        logWindow.setName("log");
         logWindow.setLocation(x,y);
-        logWindow.setSize(width, heigh);
+        logWindow.setSize(width, height);
         setMinimumSize(logWindow.getSize());
         logWindow.pack();
         Logger.debug("Протокол работает");
         return logWindow;
     }
 
+    private LogWindow createLogWindow()
+    {
+        return createLogWindow(10,10 ,300, 800);
+    }
 
-    protected GameWindow createGameWindow(int x, int y, int width, int heigh) {
+    private GameWindow createGameWindow(int x, int y, int width, int height) {
         GameWindow gameWindow = new GameWindow();
+        gameWindow.setName("game");
         gameWindow.setLocation(x,y);
-        gameWindow.setSize(width, heigh);
+        gameWindow.setSize(width, height);
         return gameWindow;
     }
 
-    protected void createDefaultPair() {
-        LogWindow logWindow = createLogWindow();
-        addWindow(logWindow);
-        logWindow.setName("0");
-        GameWindow gameWindow = new GameWindow();
-        gameWindow.setSize(400,  400);
-        addWindow(gameWindow);
-        gameWindow.setName("1");
+    private GameWindow createGameWindow() {
+        return createGameWindow(0, 0, 400, 400);
+    }
+
+    private void createDefaultPair() {
+        addWindow(createLogWindow());
+        addWindow(createGameWindow());
     }
 
     
-    protected void addWindow(JInternalFrame frame)
+    private void addWindow(JInternalFrame frame)
     {
         desktopPane.add(frame);
         frame.setVisible(true);
     }
-
-
 
     private JMenuBar generateMenuBar()
     {
@@ -167,31 +134,24 @@ public class MainApplicationFrame extends JFrame
 
         JMenu fileMenu = new JMenu("Файл");
         fileMenu.setMnemonic(KeyEvent.VK_F);
-        fileMenu.getAccessibleContext().setAccessibleDescription(
-                "Управление приложением");
-        fileMenu.add(generateMenuItem("Новая пара окон", (event) -> createDefaultPair()));
-        fileMenu.add(generateMenuItem("Выход", (event) -> closeAll()));
+        fileMenu.getAccessibleContext().setAccessibleDescription("Управление приложением");
+        generateMenuItem(fileMenu,"Новая пара окон", (event) -> createDefaultPair());
+        generateMenuItem(fileMenu,"Выход", (event) -> closeAll());
         
         JMenu lookAndFeelMenu = new JMenu("Режим отображения");
         lookAndFeelMenu.setMnemonic(KeyEvent.VK_V);
-        lookAndFeelMenu.getAccessibleContext().setAccessibleDescription(
-                "Управление режимом отображения приложения");
-        lookAndFeelMenu.add(generateMenuItem(
-                "Системная схема",
-                (event) -> { setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-                    this.invalidate(); }));
-        lookAndFeelMenu.add(generateMenuItem(
-                "Универсальная схема",
-                (event) -> { setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
-                    this.invalidate(); }));
+        lookAndFeelMenu.getAccessibleContext().setAccessibleDescription("Управление режимом отображения приложения");
+        generateMenuItem(lookAndFeelMenu,"Системная схема", (event) ->
+            { setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            this.invalidate(); });
+        generateMenuItem(lookAndFeelMenu,"Универсальная схема", (event) ->
+            { setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+            this.invalidate(); });
 
         JMenu testMenu = new JMenu("Тесты");
         testMenu.setMnemonic(KeyEvent.VK_T);
-        testMenu.getAccessibleContext().setAccessibleDescription(
-                "Тестовые команды");
-        testMenu.add(generateMenuItem(
-                "Сообщение в лог",
-                (event) -> Logger.debug("Новая строка")));
+        testMenu.getAccessibleContext().setAccessibleDescription("Тестовые команды");
+        generateMenuItem(testMenu,"Сообщение в лог", (event) -> Logger.debug("Новая строка"));
 
         menuBar.add(fileMenu);
         menuBar.add(lookAndFeelMenu);
@@ -199,23 +159,20 @@ public class MainApplicationFrame extends JFrame
         return menuBar;
     }
 
-    private JMenuItem generateMenuItem(String name, ActionListener action) {
-        JMenuItem item = new JMenuItem(name, KeyEvent.VK_S);
+    private void generateMenuItem(JMenu menu, String name, ActionListener action) {
+        JMenuItem item = new JMenuItem(name);
         item.addActionListener(action);
-        return item;
+        menu.add(item);
     }
     
     private void setLookAndFeel(String className)
     {
-        try
-        {
+        try {
             UIManager.setLookAndFeel(className);
             SwingUtilities.updateComponentTreeUI(this);
         }
         catch (ClassNotFoundException | InstantiationException
             | IllegalAccessException | UnsupportedLookAndFeelException e)
-        {
-            // just ignore
-        }
+        { /* Just ignore */ }
     }
 }
