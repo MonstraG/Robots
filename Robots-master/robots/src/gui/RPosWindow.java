dca5edc7d6d80559b44c7aa6359f1d4ded8b8eb1
Arseniy Garelyshev <Ars.garelyshev@yandex.ru>
2018-04-14 09:11:00 +0500

    expanded gitignore

--- Robots-master/robots/src/gui/RPosWindow.java
+++ Robots-master/robots/src/gui/RPosWindow.java
@@ -1,8 +1,14 @@
 package gui;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class RPosWindow {
     public RPosWindow()
     {
+        JPanel panel = new JPanel(new BorderLayout());
+        panel.add(JOptionPane.showMessageDialog(this, JOptionPane.INFORMATION_MESSAGE));
         //TODO: Window that shows robot coordinates
+
     }
 }
