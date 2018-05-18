deec6dea8cb0d89a6d06babd3c43397755b3dde0
MonstraG <Ars.garelyshev@yandex.ru>
2018-05-18 13:51:36 +0500

    Can now remove robots.

--- Robots-master/robots/src/gui/GameWindow.java
+++ Robots-master/robots/src/gui/GameWindow.java
@@ -58,8 +58,24 @@ public class GameWindow extends JInternalFrame
             }
         });
 
+        JButton removeRobot = new JButton(); //remove robot
+        try {
+            Image img = ImageIO.read(getClass().getResource("/img/minus.png"));
+            removeRobot.setIcon(new ImageIcon(img));
+        } catch (Exception ex) {
+            System.out.println(ex.toString());
+        }
+        removeRobot.setMnemonic(KeyEvent.VK_D);
+        removeRobot.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent mouseEvent) {
+                removeLastRobot();
+            }
+        });
+
         menu.add(pause);
         menu.add(addRobot);
+        menu.add(removeRobot);
 
         this.setJMenuBar(menu);
 
@@ -72,6 +88,11 @@ public class GameWindow extends JInternalFrame
 
     void addNewRobot() {
         robotList.add( new RobotMovement(this));
+    }
+
+    void removeLastRobot() {
+        if (robotList.size() > 0)
+            robotList.remove(robotList.size() - 1);
     }
 
     ArrayList<RobotMovement> getRobots() { return robotList; }
